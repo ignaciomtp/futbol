9869522abe81f70da62a40eb34a06545c3193849
ignaciomtp <ignaciomtp@gmail.com>
2025-03-29 12:42:11 +0100

    Página inicio

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -5,17 +5,23 @@ import { ref, onMounted } from 'vue';
 import { router, Link, usePage } from '@inertiajs/vue3';
 import PlayerContainer from '@/Components/PlayerContainer.vue';
 import NavigationBar from '@/Components/NavigationBar.vue';
-//import i18n from 'laravel-vue-i18n/vite'; 
+import HomeCover from '@/Components/HomeCover.vue';
 import { loadLanguageAsync } from 'laravel-vue-i18n';
 
 let props = defineProps({ footble: Number });
 
 const page = usePage();
 
+const playGame = ref(false); 
+
 const searchQuery = ref('');
 const suggestions = ref([]);
 const showSuggestions = ref(false);
 const guesses = ref([]);
+
+const playTheGame = () => {
+    playGame.value = true;
+}
 
 const searchPlayers = async () => {
   if (searchQuery.value.length < 1) {
@@ -120,37 +126,49 @@ onMounted(() => {
 <template>
   <NavigationBar @locale-changed="changeLocale" />
 
-  <main class="container text-bg-dark p-4">
+  <main class="container text-bg-dark mt-5 p-4">
     <div class="row pt-4">
       <div class="col-md-3 text-center">
         <p> {{ $t('left column') }}</p>
       </div>
       <div class="col-md-6">
-        <div class="guesses-remaining">{{ $t('Guess') + ' ' + (guesses.length + 1) + ' ' + $t('of') }} 10</div>
-        <div class="input-group mb-3 input-dropdown-container pl-5 pr-5">
-          <input type="text" class="searchbox" 
-            :placeholder="$t('Type a footballer name here') + '...'" 
-            v-model="searchQuery" 
-            @input="searchPlayers"
-            autocomplete="off">
-          <span class="searchbox-button">
-            <i class="bi bi-search text-bg-light"></i>
-          </span>
-          <div class="dropdown w-100">
-            <ul class="dropdown-menu" id="suggestions" v-if="showSuggestions">
-              <li v-for="player in suggestions" :key="player.id">
-                <div class="dropdown-item dropdown-player-item" @click="selectPlayer(player)">
-                  <img :src="`/img/players/${player.photo}`" :alt="player.name" class="tinythumb" style="float: right">
-                  {{ player.name }}
-                </div>
-              </li>
-            </ul>
-          </div>
+
+        <HomeCover v-if="!playGame" @play-game="playTheGame" />
+
+        <div id="game-container" v-if="playGame">
+            <div class="guesses-remaining">{{ $t('Guess') + ' ' + (guesses.length + 1) + ' ' + $t('of') }} 10</div>
+            <div class="input-group mb-3 input-dropdown-container pl-5 pr-5">
+              <input type="text" class="searchbox" 
+                :placeholder="$t('Type a footballer name here') + '...'" 
+                v-model="searchQuery" 
+                @input="searchPlayers"
+                autocomplete="off">
+              <span class="searchbox-button">
+                <i class="bi bi-search text-bg-light"></i>
+              </span>
+              <div class="dropdown w-100">
+                <ul class="dropdown-menu" id="suggestions" v-if="showSuggestions">
+                  <li v-for="player in suggestions" :key="player.id">
+                    <div class="dropdown-item dropdown-player-item" @click="selectPlayer(player)">
+                      <img :src="`/img/players/${player.photo}`" :alt="player.name" class="tinythumb" style="float: right">
+                      {{ player.name }}
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="mt-3 text-center" v-if="!guesses.length">
+                <p class="m-4">{{ $t('Guess the footballer of the day') }}.</p>
+                <p class="m-4">{{ $t('Search for an footballer to make your first guess') }}.</p>
+            </div>
+
+            <div class="mt-5" id="guesses">
+              <PlayerContainer v-for="player in guesses" :key="player.id" :player="player" />
+            </div>
         </div>
 
-        <div class="mt-5" id="guesses">
-          <PlayerContainer v-for="player in guesses" :key="player.id" :player="player" />
-        </div>
+
       </div>
       <div class="col-md-3 text-center">
         <p> {{ $t('right column') }}</p>
