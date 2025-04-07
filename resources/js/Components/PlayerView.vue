b213a9991b93abdbcba36bdaa52b0dd7ff69a56a
ignaciomtp <ignaciomtp@gmail.com>
2025-04-07 12:09:16 +0200

    Página Create

---
+++ resources/js/Components/PlayerView.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  player: Object,
+});
+</script>
+<template>
+	<div class="d-flex align-items-center ancho ">
+	    <img :src="'/img/players/' + player.photo" :alt="player.name" class="round-thumb">
+	    <h2 class="text-bg-dark mb-0">{{ player.name }}</h2>
+	</div>
+</template>
+<style scoped>
+.ancho {
+	width: 100%;
+	justify-content: center;
+}
+</style>
